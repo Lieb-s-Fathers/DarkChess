01d01995867f5a111569c76fb8d22e9becd3be78
Zaoerdd <zaoerdd@gmail.com>
2022-11-27 23:55:44 +0800

    feat: 完善AI控制逻辑

---
+++ src/AI/AI.java
@@ -0,0 +1,56 @@
+package AI;
+
+import chessComponent.SquareComponent;
+import model.ChessColor;
+import view.Chessboard;
+
+public class AI {
+    public static int[][] components = new int[10][5];
+    public static int[][] colors = new int[10][5];
+    public static int nowColor;
+
+    static int[][] componentCounter = new int[10][5];
+    static SquareComponent[][] squareComponents;
+    Chessboard chessboard;
+
+    //统计棋子个数
+    protected static int[][] countComponents() {
+        int[][] componentCounter = new int[10][2];
+        for (int x = 0; x < 7; x++)
+            for (int y = 0; y < 4; y++)
+                if (colors[x][y] == nowColor) componentCounter[components[x][y]][colors[x][y]]++;
+        return componentCounter;
+    }
+
+    public void loadChessboard(Chessboard chessboard) {
+        //加载棋盘
+        squareComponents = chessboard.getChessComponents();
+        for (int x = 0; x < 7; x++) {
+            for (int y = 0; y < 4; y++) {
+                components[x][y] = squareComponents[x][y].getStyle();
+                colors[x][y] = squareComponents[x][y].getChessColor() == ChessColor.BLACK ? 0 : 1;
+            }
+        }
+        //统计棋子个数
+        componentCounter = countComponents();
+    }
+
+    public int[][] getCanMovePoints(SquareComponent squareComponent) {
+        int i = 0;
+        int[][] canMovePoints = {{-1, -1, -1, -1}, {-1, -1, -1, -1}, {-1, -1, -1, -1}, {-1, -1, -1, -1}, {-1, -1, -1, -1}};
+        SquareComponent[][] squareComponents = chessboard.getChessComponents();
+        for (int x = 0; x < 7; x++) {
+            for (int y = 0; y < 4; y++) {
+                if (squareComponent.canMoveTo(squareComponents, squareComponents[x][y].getChessboardPoint())) {
+                    canMovePoints[i][0] = x;
+                    canMovePoints[i++][1] = y;
+                }
+            }
+        }
+        if (!squareComponent.isReversal()) {
+            canMovePoints[0][0] = squareComponent.getChessboardPoint().getX();
+            canMovePoints[0][1] = squareComponent.getChessboardPoint().getY();
+        }
+        return canMovePoints;
+    }
+}
---
+++ src/AI/AIController.java
@@ -0,0 +1,92 @@
+package AI;
+
+import view.Chessboard;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.KeyEvent;
+
+
+public class AIController extends JFrame {
+
+
+    private final Robot robot;
+    private final Chessboard chessboard;
+
+
+    public AIController(Chessboard chessboard) {
+        this.chessboard = chessboard;
+        try {
+            robot = new Robot();
+        } catch (AWTException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void play() {
+        RandomAI randomAI = new RandomAI();
+        System.out.println(chessboard);
+        int[] xy = randomAI.move(chessboard);
+        this.pressComponent(xy[2], xy[3]);
+        this.pressComponent(xy[0], xy[1]);
+    }
+
+
+    public  void pressComponent(int row, int col) {
+        if (row >= 0 && col >= 0) {
+            Point directionPoint = this.findDirectionPoint(row, col);
+            try {
+                this.AIpress(directionPoint.x, directionPoint.y);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    // 获取当前的鼠标位置
+    public Point getNowpoint() {
+        return MouseInfo.getPointerInfo().getLocation();
+    }
+
+    public void press() {
+        robot.mousePress(KeyEvent.BUTTON1_DOWN_MASK);
+        robot.mouseRelease(KeyEvent.BUTTON1_DOWN_MASK);
+    }
+
+    public void moveTo(Point point) {
+        robot.mouseMove(point.x, point.y);
+    }
+
+    public void AIpress(int x, int y) throws InterruptedException {
+        Point nowPoint = getNowpoint();
+        Point directionPoint = findDirectionPoint(x, y);
+        directionPoint.x = x;
+        directionPoint.y = y;
+        moveTo(directionPoint);
+//        Thread.sleep(100);
+        press();
+ //       Thread.sleep(100);
+        moveTo(nowPoint);
+//        Thread.sleep(100);
+    }
+
+    //获取棋盘上特定棋子的相对坐标
+    private Point calculatePoint(int row, int col) {
+        return new Point(col * (600) / 8 + 3, row * (600) / 8 + 3);
+    }
+
+    private Point findDirectionPoint(int row, int col) {
+        //获取屏幕中心点坐标
+        Dimension dim = getToolkit().getScreenSize();
+
+        int x0 = dim.width / 2;
+        int y0 = dim.height / 2;
+        //初始位置窗口左上角坐标
+        int x1 = x0 - 360;
+        int y1 = y0 - 360;
+        //获取窗口内相对坐标
+        Point point1 = calculatePoint(row, col);
+        return new Point(point1.x + x1 + 100, point1.y + y1 + 100);
+    }
+
+}
